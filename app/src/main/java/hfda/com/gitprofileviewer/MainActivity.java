457b65fb116a5ca4ae330e36a5a4e81fdf6a7fe3
LoneWolf <saurabhdalakoti07@gmail.com>
2019-10-10 22:21:34 +0530

    recycler view corrected, layout manager added

--- app/src/main/java/hfda/com/gitprofileviewer/MainActivity.java
+++ app/src/main/java/hfda/com/gitprofileviewer/MainActivity.java
@@ -2,6 +2,7 @@ package hfda.com.gitprofileviewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
@@ -34,16 +35,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        tvResult=(TextView)findViewById(R.id.tvResults);
         btnSearch=(Button) findViewById(R.id.btnSearch);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         etUser=(EditText) findViewById(R.id.etUser);
+
+//        setting this layout manager is important otherwise things won't work
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String userName=etUser.getText().toString();
-//                Toast.makeText(this, userName, Toast.LENGTH_SHORT).show();
                 makeAPICall(userName);
             }
         });
@@ -51,24 +53,23 @@ public class MainActivity extends AppCompatActivity {
 
     void makeAPICall(String userName){
         // Instantiate the RequestQueue.
+        // volley itself make a async call, making a request queue
         RequestQueue queue = Volley.newRequestQueue(this);
         String url ="https://api.github.com/search/users?q=";
         url=url+userName;
-        Toast.makeText(this, url, Toast.LENGTH_SHORT).show();
-// Request a string response from the provided URL.
-        System.out.println(url);
+        githubUsers.clear();
+
+        // Request a string response from the provided URL.
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
 
-//                            Toast.makeText(MainActivity.this, "Line 56", Toast.LENGTH_SHORT).show();
                         try {
                             TotalSearchResults=response.getInt("total_count");
                             JSONArray userList=response.getJSONArray("items");
                             for(int i=0;i<userList.length();i++){
                                 JSONObject temp=userList.getJSONObject(i);
-//                                tvResult.append(temp.getString("login")+"\n");
                                 githubUsers.add(new users(temp.getString("avatar_url"),temp.getString("login"),temp.getString("html_url"),temp.getString("score")));
                             }
                         } catch (JSONException e) {
@@ -84,11 +85,6 @@ public class MainActivity extends AppCompatActivity {
         });
         // Add the request to the RequestQueue.
         queue.add(request);
-        Toast.makeText(this, Integer.toString(githubUsers.size()), Toast.LENGTH_SHORT).show();
-        for(int i=0;i<githubUsers.size();i++){
-//            System.out.println(githubUsers.get(i));
-            Toast.makeText(this,"hmm", Toast.LENGTH_SHORT).show();
-        }
         adapter=new customAdapter(githubUsers,this);
         recyclerView.setAdapter(adapter);
     }
--- app/src/main/java/hfda/com/gitprofileviewer/customAdapter.java
+++ app/src/main/java/hfda/com/gitprofileviewer/customAdapter.java
@@ -3,6 +3,7 @@ package hfda.com.gitprofileviewer;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,8 @@ import java.util.ArrayList;
 public class customAdapter extends RecyclerView.Adapter<customAdapter.ViewHolder>{
     private ArrayList<users> gitUsers=new ArrayList<>();
     private Context context;
+
+    // this ViewHolder internal class define each view in the itemView and populate the values in it when it is called by the recyclerView
     public class ViewHolder extends RecyclerView.ViewHolder{
         private TextView url,userName,score;
         private ImageView avatarUrl;
@@ -30,11 +33,13 @@ public class customAdapter extends RecyclerView.Adapter<customAdapter.ViewHolder
         }
     }
 
+    // declaring the constructor
     public customAdapter(ArrayList<users> users,Context context){
         this.gitUsers=users;
         this.context=context;
     }
 
+    // this would create a viewHolder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
@@ -42,16 +47,18 @@ public class customAdapter extends RecyclerView.Adapter<customAdapter.ViewHolder
         return new ViewHolder(v);
     }
 
+    // this set value of each item in recyclerView
     @Override
     public void onBindViewHolder(@NonNull customAdapter.ViewHolder myViewHolder, int i) {
         users user=gitUsers.get(i);
-//        Picasso.with(context).load(user.getAvatarUrl()).into(myViewHolder.avatarUrl);
-//        myViewHolder.avatarUrl.setImageResource("R.drawable.ic");
+        Picasso.with(context).load(user.getAvatarUrl()).into(myViewHolder.avatarUrl);
         myViewHolder.score.setText(user.getScore());
         myViewHolder.url.setText(user.getHtml_url());
         myViewHolder.userName.setText(user.getLogin());
+        Log.d("mmm",user.getLogin());
     }
 
+    // return the item count
     @Override
     public int getItemCount() {
         return gitUsers.size();
